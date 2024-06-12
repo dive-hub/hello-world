bbdc985fca16a5bfff01d1e5a5e3234b3001e0a0
unknown <iamblessedshammah@gmail.com>
2024-06-12 14:31:31 +0300

    React Training

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload ASAP!!!.
+          React Js Training whatsapp +254796566475 ASAP.
         </p>
         <a
           className="App-link"

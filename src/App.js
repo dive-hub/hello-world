38d6db7bce0ddc0e40b89560fedf7314c532c6e4
Blessed Shammah <51369113+Blessed-Shammah@users.noreply.github.com>
2024-07-04 17:07:22 +0300

    Video Like

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// Example video object
+const video = {
+  url: 'https://example.com/video',
+  title: 'Example Video Title',
+  description: 'This is an example description of the video.'
+};
+
+// Assuming Thumbnail and LikeButton components are defined elsewhere
+const Thumbnail = ({ video }) => (
+  <div>
+    <img src={video.thumbnail} alt={video.title} />
+  </div>
+);
+
+const LikeButton = ({ video }) => (
+  <button>Like</button>
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <Thumbnail video={video} />
+          <a href={video.url}>
+            <h3>{video.title}</h3>
+            <p>{video.description}</p>
+          </a>
+          <LikeButton video={video} />
+        </div>
         <p>
-          <div>
-            <Thumbnail video={video} />
-            <a href={video.url}>
-              <h3>{video.title}</h3>
-              <p>{video.description}</p>
-            </a>
-            <LikeButton video={video} />
-          </div>
           React Js Training whatsapp +254796566475 ASAP.
         </p>
         <a
